7fd9fac231a1ffdd6e5ade8b983dc5be04158200
LucieneFagundes <lfnascimento99@gmail.com>
2023-02-23 17:43:12 -0300

    correction bug

--- src/services/User/EditUserService.ts
+++ src/services/User/EditUserService.ts
@@ -1,44 +1,43 @@
 import { hash } from "bcryptjs";
+import e from "express";
 import { prisma } from "../../prisma";
 
-
 interface IEditUser {
-    id: string;
-    name?: string;
-    photo?: string;
-    email?: string;
-    password?: string;
+  id: string;
+  name?: string;
+  photo?: string;
+  email?: string;
+  password?: string;
 }
 
 export class EditUserService {
-    
-    async execute({ id, name, photo, email, password }: IEditUser) {
-        if(email){ 
-            const tempUser = await prisma.user.findFirst({
-                where: { 
-                    email, 
-                },
-            })
+  async execute({ id, name, photo, email }: IEditUser) {
+    if (email) {
+      const tempUser = await prisma.user.findFirst({
+        where: {
+          email: email,
+        },
+      });
 
-            const alreadyExists = tempUser.id != id ? true : false
-    
-            if(alreadyExists){
-                throw Error("Email already exists")
-            }
-        }
-        // todo : comparar senha anterior com nova senha
-        const passwordHash = await hash(password, 8);
+      const alreadyExists = tempUser.id === id ? true : false;
 
-        const user = await prisma.user.updateMany({
-            where: {id},
-            data: {
-                name,
-                photo,
-                email,
-                password: passwordHash
-            },
-        })
-
-        return user;
+      if (alreadyExists) {
+        throw Error("Email already exists");
+      }
     }
+    // todo : comparar senha anterior com nova senha
+    // const passwordHash = await hash(password, 8);
+
+    const user = await prisma.user.updateMany({
+      where: { id },
+      data: {
+        name,
+        photo,
+        email,
+        // password: passwordHash,
+      },
+    });
+
+    return user;
+  }
 }
